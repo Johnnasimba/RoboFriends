7bfc0ca1b5186e485117efcc5b837a4e71d8a1de
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-20 00:42:42 +0200

    Fetched some dome data from jsonplaceholder api to make the app more realistic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            robots,
+            robots:[],
             searchField: ''
          }
     }
@@ -16,17 +15,33 @@ class App extends Component {
             searchField: e.target.value
         })
     }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(Response => Response.json())
+            .then(users => {
+                this.setState({
+                    robots : users
+                })
+        })
+        
+    }
+    
     render() { 
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase()); 
         })
-        return ( 
-            <div className="tc">
-                <h1 >RoboFriends </h1>
-                <SearchBox handleChange={this.handleChange} />
-                <CardList robots={filteredRobots}  />
-            </div>
-         );
+        if (this.state.robots === 0) {
+            return <div>Loading</div>
+        } else {
+            return ( 
+                <div className="tc">
+                    <h1 >RoboFriends </h1>
+                    <SearchBox handleChange={this.handleChange} />
+                    <CardList robots={filteredRobots}  />
+                </div>
+             );
+        }
+        
     }
 }
  
