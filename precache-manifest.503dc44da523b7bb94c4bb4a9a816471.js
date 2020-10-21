e00062a1a26bc17145f1679ffc0e0b08ee970c99
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-21 23:00:46 +0200

    Updates

---
+++ precache-manifest.503dc44da523b7bb94c4bb4a9a816471.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d759e5040194e460730eaa2a39a4bb",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3628410ee7169532eeb3",
+    "url": "/RoboFriends/static/css/main.bd8c640e.chunk.css"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/RoboFriends/static/js/2.3788da92.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/RoboFriends/static/js/2.3788da92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3628410ee7169532eeb3",
+    "url": "/RoboFriends/static/js/main.8719ba4e.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
