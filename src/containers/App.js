a5d83e90a18c9f65e2feec091a3d7bdf1019c4c3
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-21 09:44:55 +0200

    Cleaned and dried the code and make it more modular

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            robots:[],
+            searchField: ''
+         }
+    }
+    handleChange = (e) => {
+        this.setState({
+            searchField: e.target.value
+        })
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(Response => Response.json())
+            .then(users => {
+                this.setState({
+                    robots : users
+                })
+        })
+        
+    }
+    
+    render() { 
+        const {robots, searchField} = this.state
+        const filteredRobots = robots.filter(robots => {
+            return robots.name.toLowerCase().includes(searchField.toLowerCase()); 
+        })
+        return (
+                robots ?
+                    <div className="tc">
+                        <h1 >RoboFriends </h1>
+                        <SearchBox handleChange={this.handleChange} />
+                        <Scroll>
+                            <CardList robots={filteredRobots} />
+                        </Scroll>
+                    </div>: (
+                   <div>Loading</div>
+                )
+        )}
+    
+}
+ 
+export default App;
