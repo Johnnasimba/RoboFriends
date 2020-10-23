195b411ed32ed3d0168b5baa8b86cb93a93d4c3f
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-23 12:52:29 +0200

    Converted class components to a function component to use hooks for state management.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary'
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            robots:[],
-            searchField: ''
-         }
-    }
-    handleChange = (e) => {
-        this.setState({
-            searchField: e.target.value
-        })
-    }
-    componentDidMount() {
+function App () {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(Response => Response.json())
-            .then(users => {
-                this.setState({
-                    robots : users
-                })
-        })
-        
-    }
+            .then(users => { setRobots(users) }); 
+    }, [])
     
-    render() { 
-        const {robots, searchField} = this.state
-        const filteredRobots = robots.filter(robots => {
+    function handleChange (e) {
+        setSearchField(e.target.value );
+    }
+    const filteredRobots =  robots.filter(robots => {
             return robots.name.toLowerCase().includes(searchField.toLowerCase()); 
         })
         return (
-                robots ?
-                    <div className="tc">
-                        <h1 >RoboFriends </h1>
-                        <SearchBox handleChange={this.handleChange} />
-                        <Scroll>
-                            <ErrorBoundary>
-                                <CardList robots={filteredRobots} />
-                            </ErrorBoundary>
-                        </Scroll>
-                    </div>: (
-                   <div>Loading</div>
-                )
-        )}
-    
+        !robots.length ?<div>Loading</div>:
+            (
+                <div className="tc">
+                <h1 >RoboFriends </h1>
+                <SearchBox handleChange={handleChange} />
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
+            )
+        )
 }
  
 export default App;
