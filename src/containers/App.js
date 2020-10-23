21f479cdb2cc66f32a4658f68198434cfa49bab6
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-23 21:44:01 +0200

    Fetched robots API using redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
-import { setSearchField } from '../actions';
+import { requestRobots, setSearchField } from '../actions';
 
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        handleChange: (e) => dispatch(setSearchField(e.target.value))
+        handleChange: (e) => dispatch(setSearchField(e.target.value)),
+        onRequestRobots: ()=> dispatch(requestRobots())
     }
 }
 
-
 function App (props) {
-    const [robots, setRobots] = useState([]);
-    
+    const { searchField, handleChange, robots, isPending, onRequestRobots } = props;
+
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(Response => Response.json())
-            .then(users => { setRobots(users) }); 
-        
+        onRequestRobots()      
     }, [])
     
-    const { searchField, handleChange } = props;
     const filteredRobots =  robots.filter(robots => {
             return robots.name.toLowerCase().includes(searchField.toLowerCase()); 
     })
    
         return (
-        !robots.length ?<div>Loading</div>:
+        isPending ?<div>Loading</div>:
             (
                 <div className="tc">
                 <h1 >RoboFriends </h1>
